8ae83e5c592209bf8e86fa57d1ea735d52783093
George Musker <georgemusk@msn.com>
2023-04-16 10:09:40 +0100

    clean server response

--- backend/src/routes/time.ts
+++ backend/src/routes/time.ts
@@ -7,15 +7,6 @@ router.get("/", function (_, res) {
   const serverTimeSeconds = new Date().getTime() / 1000;
   res.send({
     epoch: serverTimeSeconds,
-    properties: {
-      epoch: {
-        description:
-          "The current server time, in epoch seconds, at time of processing the request.",
-        type: "number",
-      },
-    },
-    required: ["epoch"],
-    type: "object",
   });
 });
 
