550ef61afde8534cee3b672a3701ab82e1328c82
George Musker <georgemusk@msn.com>
2023-04-16 09:56:29 +0100

    get metrics

--- frontend/src/components/Metrics.tsx
+++ frontend/src/components/Metrics.tsx
@@ -1,3 +1,27 @@
+import { useQuery } from "@tanstack/react-query";
+
+async function getMetrics() {
+  const res = await fetch("http://localhost:3000/metrics");
+  if (res.status !== 200) {
+    throw new Error("Server Failure");
+  }
+  return res.text();
+}
+
 export function Metrics() {
-  return <div></div>;
+  const { isLoading, data } = useQuery({
+    queryKey: ["metrics"],
+    queryFn: getMetrics,
+    refetchInterval: 30000,
+  });
+
+  if (isLoading) {
+    return <span>Loading...</span>;
+  }
+
+  return (
+    <div>
+      <pre>{data}</pre>
+    </div>
+  );
 }
