8a4ab525f1cfd02837e4942a255021cb887a5c68
George Musker <georgemusk@msn.com>
2023-04-16 09:27:53 +0100

    get server time and client time

--- frontend/src/components/TimeDifference.tsx
+++ frontend/src/components/TimeDifference.tsx
@@ -1,3 +1,42 @@
+import { useQuery } from "@tanstack/react-query";
+import moment from "moment";
+import { useEffect, useState } from "react";
+
+async function getServerTime() {
+  const res = await fetch("http://localhost:3000/time");
+  if (res.status !== 200) {
+    throw new Error("Server Failure");
+  }
+  return res.json();
+}
+
 export function TimeDifference() {
-  return <div></div>;
+  const [clientTime, setClientTime] = useState(moment());
+  useEffect(() => {
+    setInterval(() => setClientTime(moment()), 1000);
+  }, []);
+
+  const { isLoading, data } = useQuery({
+    queryKey: ["serverTime"],
+    queryFn: getServerTime,
+    onSuccess: () => setClientTime(moment()),
+  });
+
+  if (isLoading) {
+    return <span>Loading...</span>;
+  }
+
+  const { epoch: serverTimeSeconds } = data;
+  const timeDifference = moment.utc(
+    clientTime.diff(moment.unix(serverTimeSeconds))
+  );
+
+  const formattedTime = timeDifference.format("HH:mm:ss");
+
+  return (
+    <div>
+      <p>{serverTimeSeconds}</p>
+      <p>{formattedTime}</p>
+    </div>
+  );
 }
